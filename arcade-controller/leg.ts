c0047c7f689fd66e5f89a39342df50cf115caaf6
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2021-10-25 17:19:05 +0000

    Update

--- arcade-controller/leg.ts
+++ arcade-controller/leg.ts
@@ -20,23 +20,27 @@ const { subtract, union } = booleans;
 const { degToRad } = utils;
 const { extrudeLinear } = extrusions;
 
-const pillar = cylinder({ height: 20, radius: 5 });
-const bottom = cuboid({ size: [20, 50, 3] });
+const height = 6;
+const pillar = cylinder({ height, radius: 14 / 2 });
 const hole = cylinder({
   height: 3 * 1.5,
   radius: 4.5 / 2,
 });
 const chamfer = cylinderElliptic({
-  height: 0.5,
+  height: 0.7,
   startRadius: [4.5 / 2, 4.5 / 2],
   endRadius: [5.5 / 2, 5.5 / 2],
 });
 
 export const main = () => {
   return subtract(
-    union(pillar, translate([0, 15, -20 / 2 + 3 / 2], bottom)),
+    union(
+      pillar,
+      translate([0, 60 / 2, -3 / 2], cuboid({ size: [7, 60, 3] })),
+      translate([0, 60, 0], pillar)
+    ),
     translate(
-      [0, 0, 20 / 2 - (3 * 1.5) / 2],
+      [0, 0, height / 2 - (3 * 1.5) / 2],
       union(hole, translate([0, 0, (3 * 1.5 - 0.5) / 2], chamfer))
     )
   );
