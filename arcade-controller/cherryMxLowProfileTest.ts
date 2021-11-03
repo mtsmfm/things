f3c76079acf7bb447213c36661a6f95b09763b2f
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2021-11-03 03:28:45 +0000

    wip

--- arcade-controller/cherryMxLowProfileTest.ts
+++ arcade-controller/cherryMxLowProfileTest.ts
@@ -6,26 +6,21 @@ import {
   extrusions,
 } from "@jscad/modeling";
 
-const {
-  cuboid,
-  cylinder,
-  cylinderElliptic,
-  roundedCuboid,
-  polygon,
-} = primitives;
+const { cuboid, cylinder, cylinderElliptic, roundedCuboid, polygon } =
+  primitives;
 const { rotate, translate } = transforms;
 const { subtract, union } = booleans;
 const { degToRad } = utils;
 const { extrudeLinear } = extrusions;
 
 const cherryMxlowProfileHole = union(
-  cuboid({ size: [15.1, 15.1, 2], center: [0, 0, 1] }),
+  cylinder({ radius: 26 / 2, height: 2, center: [0, 0, 1] }),
   cuboid({ size: [14, 14, 2], center: [0, 0, -1] }),
   cuboid({ size: [5, 1.5, 1], center: [0, 7, -(4 - 1) / 2] }),
   cuboid({ size: [5, 1.5, 1], center: [0, -7, -(4 - 1) / 2] })
 );
 
-const basePlate = cuboid({ size: [18, 18, 4] });
+const basePlate = cuboid({ size: [26, 26, 4] });
 
 export const main = () => {
   return union(subtract(basePlate, cherryMxlowProfileHole));
--- arcade-controller/pcb-test.ts
+++ arcade-controller/pcb-test.ts
@@ -6,6 +6,8 @@ import {
   extrusions,
 } from "@jscad/modeling";
 
+import { main as cherryMxLowProfileTestFunc } from "./cherryMxLowProfileTest";
+
 const {
   cuboid,
   cylinder,
@@ -20,12 +22,12 @@ const { subtract, union } = booleans;
 const { degToRad } = utils;
 const { extrudeLinear } = extrusions;
 
-const base = cuboid({ size: [15, 15, 3] });
+const base = cuboid({ size: [18, 18, 3] });
 
 const clearance = 0.2;
 
 const cherryMxlowProfileBase = union(
-  cylinder({ radius: (5 + clearance) / 2, height: 3 }),
+  cylinder({ radius: (6 + clearance) / 2, height: 3 }),
   cylinder({ radius: (2 + clearance) / 2, height: 2, center: [0, 4.3, -0.5] }),
   rotate(
     [0, 0, degToRad(90)],
@@ -37,30 +39,93 @@ const cherryMxlowProfileBase = union(
 const socket = rotate(
   [0, degToRad(180), degToRad(90)],
   union(
-    cuboid({ size: [1.8, 1, 2], center: [0, 0, 0.25] }),
-    cuboid({ size: [1.8, 2, 1.5], center: [0, 1.8 / 2 + 1.8, 0] })
+    cuboid({
+      size: [1.8 + clearance, 1.2 + clearance, 2],
+      center: [0, 0, 0.25],
+    }),
+    cuboid({
+      size: [1.8 + clearance, 2.3 + clearance, 1.5],
+      center: [0, 1.8 / 2 + 2.2, 0],
+    }),
+    cuboid({
+      size: [0.6, 2.2, 2],
+      center: [0.7, 1.4, 0],
+    }),
+    cuboid({
+      size: [0.6, 2.2, 2],
+      center: [-0.7, 1.4, 0],
+    }),
+    cylinder({ radius: 2, height: 1.5, center: [-0.5, 4, 0] })
   )
 );
 
-const pin = cuboid({ size: [1, 0.5, 3] });
+const pin = cuboid({ size: [1.2 + clearance, 0.8 + clearance, 3] });
+
+const pcbTest = union(
+  rotate(
+    [degToRad(180), 0, degToRad(90)],
+    translate([0, 0, (4 + 3) / 2], cherryMxLowProfileTestFunc())
+  ),
+  subtract(
+    base,
+    translate([0, 6.1, (3 - 1.5) / 2], socket),
+    translate([0, 6.1, 0], pin),
+    translate([3.8, 3.1, 0], pin),
+    translate([3.8, 3.1, (3 - 1.5) / 2], rotate([0, 0, degToRad(180)], socket)),
+    cherryMxlowProfileBase,
+    rotate(
+      [0, degToRad(90), 0],
+      cylinder({ radius: 0.6, height: 100, center: [-0.7, 8, 0] })
+    ),
+    rotate(
+      [0, degToRad(90), degToRad(90)],
+      cylinder({ radius: 0.6, height: 100, center: [-0.7, -7.5, 0] })
+    )
+  )
+);
+
+const proMicroHole = union(
+  subtract(
+    cuboid({ size: [26, 54, 9] }),
+    cylinder({
+      radius: 1,
+      height: 7,
+      center: [7, -8.5, -2],
+      segments: 6,
+    }),
+    cylinder({
+      radius: 1,
+      height: 7,
+      center: [-7, -8.5, -2],
+      segments: 6,
+    }),
+    cylinder({
+      radius: 1,
+      height: 7,
+      center: [7, -24.5, -2],
+      segments: 6,
+    }),
+    cylinder({
+      radius: 1,
+      height: 7,
+      center: [-7, -24.5, -2],
+      segments: 6,
+    })
+  )
+);
 
 export const main = () => {
+  // return rotate([0, 0, degToRad(180)], proMicroHole);
   return union(
-    subtract(
-      base,
-      translate([0, 6.2, (3 - 1.5) / 2], socket),
-      translate([0, 6.2, 0], pin),
-      translate([3.8, 3, 0], pin),
-      translate([3.8, 3, (3 - 1.5) / 2], rotate([0, 0, degToRad(180)], socket)),
-      cherryMxlowProfileBase
-    )
-    // subtract(
-    //   base,
-    //   translate([0, 6, 0], pin),
-    //   translate([3.8, 3, 0], pin)
-    //   // translate([0, 6, -(3 - 1.5) / 2], socket),
-    //   // translate([3.8, 3, -(3 - 1.5) / 2],rotate([0,0,degToRad(180)], socket)),
-    //   // cherryMxlowProfileBase
+    pcbTest
+    // translate(
+    //   [0, (26 + 54) / 2, 10 / 2 - 5.5],
+    //   subtract(
+    //     cuboid({ size: [26, 54, 10] }),
+    //     translate([0, 0, (10 - 9) / 2], proMicroHole)
+    //   )
     // )
+    // translate([-26, 0, 0], pcbTest),
+    // translate([26, 0, 0], pcbTest)
   );
 };
