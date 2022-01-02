3dc40ac23b8a7474147120a502726d5064e2dab4
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2022-01-02 02:18:50 +0000

    Update cat bowl stand

--- cat-bowl-stand/cat-bowl-stand.ts
+++ cat-bowl-stand/cat-bowl-stand.ts
@@ -1,49 +1,61 @@
 import {
-  primitives,
-  transforms,
-  booleans,
-  utils,
-  extrusions,
-} from "@jscad/modeling";
-import { roundCornerCuboidWithChamfer } from "../lowee/utils/utils";
-
-const {
+  extrudeFromSlices,
+  mat4,
+  geom2,
+  slice,
+  rectangle,
   cuboid,
-  cylinder,
-  cylinderElliptic,
-  roundedCuboid,
-  roundedCylinder,
-  polygon,
-  sphere,
-} = primitives;
-const { rotate, translate, rotateX } = transforms;
-const { subtract, union } = booleans;
-const { degToRad } = utils;
-const { extrudeLinear } = extrusions;
+  subtract,
+  align,
+  expand,
+  center,
+} from "../utils";
 
-const thickness = 5;
+const thickness = 4;
 const bowlSize = 100;
-const height = 50;
+const height = 81;
+
+const slices = align({ modes: ["center", "min", "center"] }, [
+  rectangle({ size: [bowlSize, 20] }),
+  rectangle({ size: [bowlSize, bowlSize] }),
+  rectangle({ size: [bowlSize, bowlSize] }),
+]);
+
+const inner = extrudeFromSlices(
+  {
+    numberOfSlices: 3,
+    callback: (_progress, index, _base) => {
+      const transformMatrix = mat4.fromTranslation(mat4.create(), [
+        0,
+        0,
+        { 0: 0, 1: height / 4, 2: height }[index]!,
+      ]);
+
+      return slice.transform(
+        transformMatrix,
+        slice.fromSides(geom2.toSides(slices[index]))
+      );
+    },
+  },
+  slices[0]
+);
 
 export const main = () => {
-  return union(
-    subtract(
-      translate(
-        [0, 0, -height / 2],
-        roundCornerCuboidWithChamfer({
-          chamferSize: 1,
-          roundRadius: 1,
-          size: [bowlSize + thickness, bowlSize + thickness, height],
-        })
-      ),
-      cuboid({ size: [bowlSize, bowlSize, height] }),
-      translate(
-        [0, 0, -70],
-        rotateX(
-          degToRad(10),
-          cuboid({ size: [bowlSize + thickness * 2, bowlSize * 2, bowlSize] })
+  return subtract(
+    center(
+      {},
+      subtract(
+        align(
+          { modes: ["center", "center", "max"] },
+          subtract(
+            expand({ delta: thickness, corners: "round", segments: 32 }, inner),
+            inner
+          ),
+          inner
         )
       )
-    )
+    ),
+    cuboid({ size: [bowlSize, 20, height * 2], center: [0, -40, 0] }),
+    cuboid({ size: [bowlSize, 50, height * 2], center: [0, 12, 0] })
   );
 };
