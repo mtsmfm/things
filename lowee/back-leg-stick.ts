811a084a92e7abe5f5d337c27dda97a2c954afcf
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2022-01-27 15:41:16 +0000

    Improve lowee

--- lowee/back-leg-stick.ts
+++ lowee/back-leg-stick.ts
@@ -1,35 +1,14 @@
-import {
-  primitives,
-  transforms,
-  booleans,
-  utils,
-  extrusions,
-} from "@jscad/modeling";
+import { cylinder, align } from "../utils";
 
-const {
-  cuboid,
-  cylinder,
-  cylinderElliptic,
-  roundedCuboid,
-  roundedCylinder,
-  polygon,
-  sphere,
-} = primitives;
-const { rotate, translate } = transforms;
-const { subtract, union } = booleans;
-const { degToRad } = utils;
-const { extrudeLinear } = extrusions;
-
-const height = 14;
-const pillar = cylinder({ height, radius: 1.2, segments: 6 });
-const hole = cylinder({
-  height,
-  radius: 0.3,
-});
+const height = 12;
+const pillar = cylinder({ height, radius: 4 / 2, segments: 6 });
 
 export const main = () => {
-  return union(
-    subtract(pillar, hole),
-    translate([0, 0, -height / 2], cylinder({ radius: 1.5, height: 0.5 }))
-  );
+  return [
+    align(
+      { modes: ["center", "center", "min"] },
+      pillar,
+      cylinder({ radius: 3, height: 0.5 })
+    ),
+  ];
 };
--- lowee/back-leg.ts
+++ lowee/back-leg.ts
@@ -1,66 +1,83 @@
 import {
-  primitives,
-  transforms,
-  booleans,
-  utils,
-  extrusions,
-} from "@jscad/modeling";
-
-const {
-  cuboid,
   cylinder,
+  subtract,
+  union,
+  translate,
+  rotate,
+  degToRad,
   cylinderElliptic,
-  roundedCuboid,
-  roundedCylinder,
-  polygon,
-  sphere,
-} = primitives;
-const { rotate, translate } = transforms;
-const { subtract, union } = booleans;
-const { degToRad } = utils;
-const { extrudeLinear } = extrusions;
+  align,
+  center,
+  hull,
+  cuboid,
+} from "../utils";
 
 const height = 7.2;
-const pillar = cylinder({ height, radius: 14 / 2, segments: 6 });
 const suctionHole = cylinder({
   height,
-  radius: (6 / 2 / Math.sqrt(3)) * 2,
+  radius: (8 / 2 / Math.sqrt(3)) * 2,
   segments: 6,
 });
 const chamferHeight = 2.2;
 const shaftHeight = 4 - chamferHeight;
 
-export const main = () => {
-  return subtract(
-    union(pillar, translate([7, 0, 0], pillar)),
-    suctionHole,
-    translate(
-      [0, 0, height / 2 - 3.25],
-      rotate([degToRad(90), 0, 0], cylinder({ height: 20, radius: 1.4 }))
-    ),
-    translate(
-      [8, 0, 0],
-      union(
-        cylinder({
-          height,
-          radius: (3 + 0.3) / 2,
+const size = 11;
+
+const suctionHolder = center(
+  {},
+  subtract(
+    align(
+      { modes: ["min", "center", "center"] },
+      subtract(
+        cuboid({
+          size: [size, size, height],
         }),
-        translate(
-          [0, 0, (height - (height - (shaftHeight + chamferHeight))) / 2],
-          cylinder({
-            height: height - (shaftHeight + chamferHeight),
-            radius: (6 + 0.3) / 2,
-          })
-        ),
-        translate(
-          [0, 0, -(height - chamferHeight) / 2 + shaftHeight],
-          cylinderElliptic({
-            height: chamferHeight,
-            startRadius: [(3 + 0.3) / 2, (3 + 0.3) / 2],
-            endRadius: [(6 + 0.3) / 2, (6 + 0.3) / 2],
-          })
-        )
+        suctionHole
+      ),
+      rotate(
+        [degToRad(90), 0, degToRad(90)],
+        cylinder({ height: 12, radius: 4 / 2 })
       )
     )
-  );
+  )
+);
+
+const screwHole = union(
+  cylinder({
+    height,
+    radius: (3 + 0.3) / 2,
+  }),
+  translate(
+    [0, 0, (height - (height - (shaftHeight + chamferHeight))) / 2],
+    cylinder({
+      height: height - (shaftHeight + chamferHeight),
+      radius: (6 + 0.3) / 2,
+    })
+  ),
+  translate(
+    [0, 0, -(height - chamferHeight) / 2 + shaftHeight],
+    cylinderElliptic({
+      height: chamferHeight,
+      startRadius: [(3 + 0.3) / 2, (3 + 0.3) / 2],
+      endRadius: [(6 + 0.3) / 2, (6 + 0.3) / 2],
+    })
+  )
+);
+
+export const main = () => {
+  return [
+    subtract(
+      union(
+        suctionHolder,
+        subtract(
+          translate(
+            [7, 0, 0],
+            cylinder({ height, radius: size / Math.sqrt(3), segments: 6 })
+          ),
+          hull(suctionHolder, suctionHolder)
+        )
+      ),
+      translate([9, 0, 0], screwHole)
+    ),
+  ];
 };
