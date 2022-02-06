5b8380132d336c19f77afd64069a480b7f18fa65
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2022-02-06 12:32:44 +0000

    Update joystick plate

--- nowa/joystick-plate.ts
+++ nowa/joystick-plate.ts
@@ -1,5 +1,85 @@
-import { joystickPlate } from "./column";
+import {
+  corners,
+  cuboid,
+  cylinder,
+  subtract,
+  translate,
+  align,
+  alignSubtract,
+  replace,
+  m2ScrewHoleRadius,
+  m2NutRadius,
+  m3InsertNutHoleRadius,
+  m2NutHeight,
+} from "../utils";
+
+const height = 4;
+const thickness = 1;
+
+const m2NutHolder = alignSubtract(
+  cylinder({
+    radius: (m2NutRadius + 2) / 2,
+    height: m2NutHeight + thickness,
+    segments: 6,
+  }),
+  [
+    { modes: ["none", "none", "max"] },
+    cylinder({
+      radius: m2NutRadius / 2,
+      height: m2NutHeight,
+      segments: 6,
+    }),
+  ],
+  [
+    { modes: ["none", "none", "min"] },
+    cylinder({
+      radius: m2ScrewHoleRadius,
+      height: 4,
+    }),
+  ]
+);
+
+const cornerHoles = corners(
+  translate(
+    [25.5 / 2 - 2.5, 31.5 / 2 - 2.5, 0],
+    subtract(
+      cylinder({ radius: m3InsertNutHoleRadius + 0.5, height }),
+      cylinder({ radius: m3InsertNutHoleRadius, height })
+    )
+  )
+);
 
 export const main = () => {
-  return [joystickPlate];
+  return replace(
+    align(
+      { modes: ["none", "none", "min"] },
+      replace(
+        alignSubtract(
+          cuboid({ size: [25.5, 31.5, height] }),
+          [
+            [
+              { modes: ["none", "max", "none"] },
+              { modes: ["none", "min", "none"] },
+            ],
+            cuboid({ size: [13, 3, height] }),
+          ],
+          [
+            { modes: ["center", "center", "max"] },
+            cuboid({ size: [24, 22, height - thickness] }),
+          ]
+        ),
+        cornerHoles
+      )
+    ),
+    [
+      align(
+        { modes: ["none", "center", "min"], relativeTo: [0, 7, 0] as any },
+        m2NutHolder
+      ),
+      align(
+        { modes: ["none", "center", "min"], relativeTo: [0, -7, 0] as any },
+        m2NutHolder
+      ),
+    ]
+  );
 };
--- utils/index.ts
+++ utils/index.ts
@@ -9,10 +9,11 @@ import {
   utils,
   colors,
   hulls,
+  measurements,
 } from "@jscad/modeling";
 export { generalize } from "@jscad/modeling/src/operations/modifiers";
 export { Vec2, Vec3 } from "@jscad/modeling/src/maths/types";
-export { Geom2, Geom3 } from "@jscad/modeling/src/geometries/types";
+import { Geom2, Geom3 } from "@jscad/modeling/src/geometries/types";
 
 export const {
   cylinder,
@@ -22,6 +23,8 @@ export const {
   sphere,
   cylinderElliptic,
   torus,
+  polygon,
+  polyhedron,
 } = primitives;
 export const {
   rotate,
@@ -30,14 +33,73 @@ export const {
   rotateZ,
   align,
   translate,
+  translateX,
+  translateY,
+  translateZ,
   center,
   mirror,
+  mirrorX,
+  mirrorY,
+  mirrorZ,
 } = transforms;
 export const { subtract, union, intersect } = booleans;
-export const { extrudeFromSlices, slice } = extrusions;
+export const { extrudeFromSlices, extrudeLinear } = extrusions;
 export const { expand } = expansions;
 export const { degToRad } = utils;
 export const { geom2 } = geometries;
 export const { mat4 } = maths;
 export const { colorize, colorNameToRgb } = colors;
 export const { hull, hullChain } = hulls;
+export const { measureBoundingBox, measureAggregateBoundingBox } = measurements;
+
+export { Geom2, Geom3 };
+
+export const corners = (obj: Geom3) => {
+  return [
+    obj,
+    mirror({ normal: [1, 0, 0] }, obj),
+    mirror({ normal: [0, 1, 0] }, obj),
+    mirror({ normal: [1, 0, 0] }, mirror({ normal: [0, 1, 0] }, obj)),
+  ];
+};
+
+export const replace = (a: Geom3, b: Geom3 | Geom3[]) => {
+  return union(
+    subtract(
+      a,
+      [b].flat().map((x) => hull(x, x))
+    ),
+    b
+  );
+};
+
+type AlignOption = Parameters<typeof align>["0"];
+
+export const alignSubtract = (
+  obj: Geom3,
+  ...args: [AlignOption | AlignOption[], Geom3][]
+) => {
+  return args.reduce((acc, [alignOptionOrOptions, obj]) => {
+    return [alignOptionOrOptions].flat().reduce((innerAcc, opt) => {
+      return center({}, subtract(align(opt, innerAcc, obj)));
+    }, acc);
+  }, obj);
+};
+
+export const onlyPositiveZ = (obj: Geom3) => {
+  const [min, max] = measureBoundingBox(obj);
+
+  const bottom = rectangle({
+    size: [
+      Math.max(Math.abs(max[0]), Math.abs(min[0])) * 2,
+      Math.max(Math.abs(max[1]), Math.abs(min[1])) * 2,
+    ],
+  });
+
+  return intersect(obj, extrudeLinear({ height: max[2] }, bottom));
+};
+
+export const m3InsertNutHoleRadius = 4.5 / 2;
+export const m2ScrewHoleRadius = 2.5 / 2;
+export const m2NutRadius = (4.6 + 0.4) / 2;
+export const m2NutHeight = 1.6 + 0.2;
